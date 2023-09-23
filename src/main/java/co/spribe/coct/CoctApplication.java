613a38ad1b2f877a45790cd23e1f2e609d9842af
Nikolai Bogdan <nbogdan@spribe.io>
2023-09-23 09:30:45 +0200

    Initial commit

---
+++ src/main/java/co/spribe/coct/CoctApplication.java
@@ -0,0 +1,13 @@
+package co.spribe.coct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/spribe/coct/CoctApplicationTests.java
@@ -0,0 +1,13 @@
+package co.spribe.coct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
